a92b1139afb0b292f10d749c03947599d86b47d0
olepavlsen <olepavlsen@mail.ru>
2024-07-25 08:26:05 +0300

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -5,10 +5,6 @@ m = 1
 for i in range(1, n + 1):
     if n % i == 0:
         divisors.append(i)
-    if 1 in divisors:
-        divisors.remove(1)
-    if 2 in divisors:
-        divisors.remove(2)
 while m <= n / 2:
     for k in range(len(divisors)):
         if divisors[k] - m > m:
